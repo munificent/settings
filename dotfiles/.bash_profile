71ab181d2dd94989e76fdd37e21f41d38b61fab3
Bob Nystrom <robert@stuffwithstuff.com>
2012-10-19 11:33:54 -0700

    Add bash completion and home-brew.

--- dotfiles/.bash_profile
+++ dotfiles/.bash_profile
@@ -6,3 +6,14 @@ export PATH="$PATH":~/dev/depot_tools
 
 # Add my live built version of the Dart SDK to path.
 export PATH="$PATH":~/dev/dart/dart/xcodebuild/ReleaseIA32/dart-sdk/bin
+
+# Add bash completion for git.
+# See: https://github.com/bobthecow/git-flow-completion/wiki/Install-Bash-git-completion
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+    . `brew --prefix`/etc/bash_completion
+fi
+
+# Set up my prompt including git hotness.
+export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWUNTRACKEDFILES=1
+export PS1='\[\033[37m\]\w$(__git_ps1 " \[\033[32m\](%s)")\[\033[0m\] → '
--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
-# Sets up symlinks for the various files in this repo.
-# Run this script from the directory containing it.
+# Automatically sets up and installs as much as possible.
+# Make sure to run this from the directory containing it.
 
-# The bash config.
+# May want to check that this is up to date at:
+# http://mxcl.github.com/homebrew/
+echo "Installing homebrew..."
+ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+
+echo "Symlinking .bash_profile..."
 ln -fs $PWD/dotfiles/.bash_profile ~/.bash_profile
 
+echo "Symlinking Sublime settings..."
 # The Sublime settings. Note: because this symlinks to the real file here,
 # Sublime won't correctly see any changes made to it and refresh itself. If you
 # modify this, just run install.sh again to poke Sublime into reloading it.
 ln -fs $PWD/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+
+echo "Installing git bash completion..."
+brew install git bash-completion
+
+# Run the new .bash_profile.
+source ~/.bash_profile
